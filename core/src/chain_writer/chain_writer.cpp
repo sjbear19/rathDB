fdc185c95e4c3d2efeca948e6a5fa4fe30be5af4
Colby Anderson <56745171+colby-anderson@users.noreply.github.com>
2021-06-23 02:49:36 -0700

    Update chain_writer.cpp

--- core/src/chain_writer/chain_writer.cpp
+++ core/src/chain_writer/chain_writer.cpp
@@ -1,6 +1,6 @@
 #include <chain_writer.h>
 
-const std::string ChainWriter::_file_extension = ".data";
+const std::string ChainWriter::_file_extension = "data";
 const std::string ChainWriter::_block_filename = "blocks";
 const uint16_t ChainWriter::_max_block_file_size = 1000;
 const std::string ChainWriter::_undo_filename = "undo_blocks";
